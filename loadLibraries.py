fe54cfb724e7683d37ee6c6948dd4bb4dedfb8ab
racdale <racdale@gmail.com>
2015-07-13 12:13:34 -0700

    update with py / notebooks

---
+++ loadLibraries.py
@@ -0,0 +1,6 @@
+from pybrain.supervised.trainers.backprop import BackpropTrainer
+from pybrain.structure import RecurrentNetwork, LinearLayer, SigmoidLayer ,TanhLayer, BiasUnit, FullConnection
+from pybrain.datasets import SupervisedDataSet 
+import matplotlib.pyplot as plt
+import numpy as np 
+from random import random as rnd
