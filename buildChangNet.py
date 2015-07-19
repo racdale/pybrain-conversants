a1e5bb6d733cd4a45ebfdb627bfca9da71542754
racdale <racdale@gmail.com>
2015-07-19 13:20:14 -0700

    Exploring reason for not learning right sequences!

--- buildChangNet.py
+++ buildChangNet.py
@@ -2,10 +2,10 @@ def buildSRN(i,h,o):
     net = RecurrentNetwork()
     # initialize layers
     net.addInputModule(LinearLayer(i,name='input'))
-    net.addModule(SigmoidLayer (h,name='hidden'))
+    net.addModule(SigmoidLayer(h,name='hidden'))
     net.addModule(BiasUnit('bias'))
-    net.addOutputModule(SigmoidLayer (o,name='output'))
-    net.addModule(LinearLayer(2,name='context'))
+    net.addOutputModule(SigmoidLayer(o,name='output'))
+    net.addModule(LinearLayer(h,name='context'))
     # connect layers
     net.addConnection(FullConnection(net['input'], net['hidden']))
     net.addConnection(FullConnection(net['context'], net['hidden']))
@@ -16,16 +16,16 @@ def buildSRN(i,h,o):
     return(net)
 
 # get the bare SRNs
-net_a_c = buildSRN(3,2,3) 
-net_a_p = buildSRN(3,2,3)
-net_b_c = buildSRN(3,2,3)
-net_b_p = buildSRN(3,2,3)
+net_a_c = buildSRN(szIn,hids,szIn) 
+net_a_p = buildSRN(szIn,hids,szIn)
+net_b_c = buildSRN(szIn,hids,szIn)
+net_b_p = buildSRN(szIn,hids,szIn)
 
 # Chang 'em
-net_a_p.addModule(LinearLayer(2,name='hidden input from comp'))
-net_a_p.addConnection(FullConnection(net_a_p['hidden input from comp'],net_a_p['hidden'])) # comp > production
-net_b_p.addModule(LinearLayer(2,name='hidden input from comp'))
-net_b_p.addConnection(FullConnection(net_b_p['hidden input from comp'],net_b_p['hidden'])) # comp > production
+#net_a_p.addModule(LinearLayer(hids,name='hidden input from comp'))
+#net_a_p.addConnection(FullConnection(net_a_p['hidden input from comp'],net_a_p['hidden'])) # comp > production
+#net_b_p.addModule(LinearLayer(hids,name='hidden input from comp'))
+#net_b_p.addConnection(FullConnection(net_b_p['hidden input from comp'],net_b_p['hidden'])) # comp > production
 # note: listening handled by training function (enforcing linear input from a > b; b > a)
 # note: transfer of activation from comp > prod done linearly in training, as well; see trainChangs.py
 
@@ -34,7 +34,10 @@ net_a_p.sortModules()
 net_b_c.sortModules()
 net_b_p.sortModules()
 
-   
+net_a_c.randomize()
+net_a_p.randomize()
+net_b_c.randomize()
+net_b_p.randomize()
     
 #net.sortModules()
 #net.randomize()
--- buildTurnInput.py
+++ buildTurnInput.py
@@ -1,31 +1,48 @@
 sentence = []
-sentence.append([1,0,0])
-sentence.append([0,1,0])
-sentence.append([0,0,1])
+sentence.append([1,0,0,0,0])
+sentence.append([0,1,0,0,0])
+sentence.append([0,0,0,0,1])
+sentence.append([0,0,1,0,0])
+sentence.append([0,0,0,1,0])
+sentence.append([0,0,0,0,1])
 
-quiet = []
-quiet.append([0,0,1])
-quiet.append([0,0,1])
-quiet.append([0,0,1])
+meaning = []
+meaning.append([1,1,0,0,0])
+meaning.append([1,1,0,0,0])
+meaning.append([1,1,0,0,0])
+meaning.append([0,0,1,1,0])
+meaning.append([0,0,1,1,0])
+meaning.append([0,0,1,1,0])
 
-ds = SupervisedDataSet(2, 2) 
-ds.addSample(sentence[0],sentence[1])
-bp = BackpropTrainer(net_a_p,ds,verbose=False,learningrate=0.25)
+preds = []
+
+for i in range(trials):
+    net_a_p.offset = 1
+    # first word
+    #ds = SupervisedDataSet(szIn, szIn) 
+    #ds.addSample(meaning[0],sentence[0])
+    #bp = BackpropTrainer(net_a_p,ds,verbose=False,learningrate=LR)
+    #bp.trainEpochs(1)
+    #listenMeaning = net_a_p.activate(meaning[0])
+    
+    #preds.append(listenMeaning)    
+    
+    for j in range(0,4):
+        net_a_p.offset = 1
+        # subsequent words
+        ds = SupervisedDataSet(szIn, szIn) 
+        ds.addSample(meaning[j],sentence[j])
+        bp = BackpropTrainer(net_a_p,ds,verbose=False,learningrate=LR)
+        bp.trainEpochs(1)
+        listenIn = net_a_p.activateOnDataset(ds)
+        preds.append(listenIn)
+        net_a_p.offset = 1
+        #listenMeaning = 3*(listenMeaning + listenIn / (listenMeaning + listenIn))
+
+        #ds = SupervisedDataSet(szIn, szIn) 
+        #ds.addSample(listenIn,listenMeaning)
+        #bp = BackpropTrainer(net_b_c,ds,verbose=False,learningrate=LR)
+        #bp.trainEpochs(1)
 
 
 
-
-
-
-ix = 0
-for i in range(100):
-    ln = int(rnd()*6)+3
-    for j in range(ln):
-        inputs.append([1,0])
-    for j in range(ln):
-        inputs.append([0,1])
-outputs = inputs[1:]
-inputs = inputs[:-1]
-ds = SupervisedDataSet(2, 2) 
-for i in range(len(inputs)):
-    ds.addSample(inputs[i],outputs[i])
